ad5cadc792375f9a587705bd3a47e69f8faa79dd
Chaitanya496-ux <chaitanyaupreti496@gmail.com>
2026-09-14 23:02:07 +0530

    LC-bag-of-tokens [Runtime: N/A; Memory: N/A]

---
+++ sorting/bag-of-tokens.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        int score = 0;
+        int left = 0;
+        int right = nums.size()-1;
+        while(left<= right){
+            if(power >= token[left]){
+                power -= token[left];
+                score += 1;
+                left += 1;
+            }else if(score < 0 ){
+                power += token[right];
+                score -= 1;
+                right -= 1;
+
+            }else{
+                break;
+            }
+        }
+        return score;
+        
+    }
+};
