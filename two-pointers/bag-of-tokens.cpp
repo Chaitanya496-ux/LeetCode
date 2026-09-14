c0f67033cfde328f0b421bbd8148565ffb041832
Chaitanya496-ux <chaitanyaupreti496@gmail.com>
2026-09-14 23:15:10 +0530

    LC-bag-of-tokens [Runtime: N/A; Memory: N/A]

--- two-pointers/bag-of-tokens.cpp
+++ two-pointers/bag-of-tokens.cpp
@@ -3,13 +3,16 @@ public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
         int score = 0;
         int left = 0;
+        int maxscore = 0;
         int right = tokens.size()-1;
+        sort(tokens.begin() , tokens.end());
         while(left<= right){
             if(power >= tokens[left]){
                 power -= tokens[left];
                 score += 1;
                 left += 1;
-            }else if(score < 0 ){
+                maxscore = max(maxscore,score);
+            }else if(score > 0 ){
                 power += tokens[right];
                 score -= 1;
                 right -= 1;
@@ -18,7 +21,7 @@ public:
                 break;
             }
         }
-        return score;
+        return maxscore;
         
     }
 };
