53f1bdb1bcd2d815ff7e0584469e6e557093fc80
Chaitanya496-ux <chaitanyaupreti496@gmail.com>
2026-09-15 23:13:55 +0530

    LC-trapping-rain-water [Runtime: N/A; Memory: N/A]

--- stack/trapping-rain-water.cpp
+++ stack/trapping-rain-water.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int left = 0; 
+        int left = 0;
         int right = height.size() -1;
         int leftmax = 0;
-        int rightmax = 0;
+        int rightmax =0;
         int res = 0;
-        while(left < right){
-            leftmax = max(leftmax, height[left]);
-            rightmax = max(rightmax, height[right]);
-            if(leftmax < rightmax){
-            res += leftmax - height[left++];
-        }else{
-            res += rightmax - height[right--];
+        while(left< right){
+            leftmax= max(leftmax , height[left]);
+            rightmax= max(rightmax , height[right]);
+            if( leftmax < rightmax){
+                res += leftmax - height[left++];
+            }else{
+                res += rightmax - height[right--];
+            }
         }
-        }
-        
         return res;
     }
 };
