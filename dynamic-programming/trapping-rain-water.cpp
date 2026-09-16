cfc5136c226dbba24d183581c4e27adc851f19ae
Chaitanya496-ux <chaitanyaupreti496@gmail.com>
2026-09-16 23:15:00 +0530

    LC-trapping-rain-water [Runtime: N/A; Memory: N/A]

--- dynamic-programming/trapping-rain-water.cpp
+++ dynamic-programming/trapping-rain-water.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int left = 0;
-        int right = height.size() -1;
+        int left =0;
+        int right = height.size()-1;
         int leftmax = 0;
-        int rightmax =0;
-        int res = 0;
+        int rightmax = 0;
+        int ans =0;
         while(left< right){
-            leftmax= max(leftmax , height[left]);
-            rightmax= max(rightmax , height[right]);
-            if( leftmax < rightmax){
-                res += leftmax - height[left++];
+            leftmax = max(leftmax, height[left]);
+            rightmax = max(rightmax, height[right]);
+            if(leftmax < rightmax){
+                ans += leftmax - height[left++];
             }else{
-                res += rightmax - height[right--];
+                ans += rightmax- height[right--];
             }
         }
-        return res;
+        return ans;
     }
 };
